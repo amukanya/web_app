659775c4aaad0c1fde79c64c1f08370aad2a38a2
amukanya <mukanya56@gmail.com>
2021-05-03 10:12:20 -0500

    added controller with HTML tags

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using web_app.Models;
+using System.Text.Encodings.Web;
+
+namespace web_app.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return Content("<h1>Hello World</h1>","text/html");
+        }
+
+        
+
+        public string Goodbye()
+        {
+            return "Goodbye!";
+        }
+    }
+}
