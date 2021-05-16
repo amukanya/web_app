d401c51d3a5497a21ce3699052edc78918c5a412
amukanya <mukanya56@gmail.com>
2021-05-16 10:04:29 -0500

    added actions to controllers

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -13,14 +13,43 @@ namespace web_app.Controllers
     public class HelloWorldController : Controller
     {
        
-
-        public IActionResult Index()
+        // GET:  /<controller>/
+        [HttpGet]
+        public IActionResult Index(string name = "World")
         {
-            return Content("<h1>Hello World</h1>","text/html");
+            //Checking name value and assigning one if not found
+            // if (string.IsNullOrEmpty(name))
+            // {
+            //     name  ="World";
+            // }
+
+            string html = "<form method = 'post'>" +
+                "<input type = 'text' name = 'name' />" +
+                "<input type = 'submit' value = 'Greet me !'/>" +
+                "</form>";
+            //return Content(html, "text/html");    
+            return Redirect("/HelloWorld/Goodbye");
         }
 
-        
+        //Hello
+        [Route("/HelloWorld")]
+        [HttpPost]
+        public IActionResult Display(string name = "World")
+        {
+            return Content(String.Format("<h1>Hello {0}</h1>",name),"text/html");
+        }
 
+        // Handle requests to /Hello/Andy(URL segement)
+        [Route("/HelloWorld/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(String.Format("<h1>Hello {0}</h1>",name),"text/html");
+        }
+
+
+        // // /Hello/Goodbye
+        // //alter the route to thid controller to be : /Hello/Aloha
+        // [Route("/Hello/Aloha")]
         public IActionResult Goodbye()
         {
             return Content("Goodbye!");
